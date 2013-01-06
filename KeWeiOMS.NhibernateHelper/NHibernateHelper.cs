4d25795fb804590b5527d73dc7357a2fa3cb5c7e
jmyd <jmyd@live.cn>
2013-01-07 00:08:04 +0800

    sds

---
+++ KeWeiOMS.NhibernateHelper/NHibernateHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate;
+using FluentNHibernate.Cfg;
+using System.IO;
+using System.Reflection;
+
+namespace KeWeiOMS.NhibernateHelper
+{
+    public class NHibernateHelper
+    {
+        private static Configuration configuration = null;
+        private static ISessionFactory sessionFactory = null;
+
+        public static void CreateConfiguration()
+        {
+            configuration = new Configuration().Configure();
+        }
+
+        public static Configuration Configuration
+        {
+            get
+            {
+                if (configuration == null)
+                {
+                    CreateConfiguration();
+                }
+                return configuration;
+            }
+            set { configuration = value; }
+        }
+
+        public static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (sessionFactory == null)
+                {
+                    if (Configuration == null)
+                    {
+                        CreateConfiguration();
+                    }
+                    FluentConfiguration fluentConfiguration = Fluently.Configure(Configuration);
+                    string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+                    string assemblyFile = Path.Combine(path, "bin/NhibernateDemo.Data.dll");
+                    fluentConfiguration.Mappings(delegate(MappingConfiguration m)
+                    {
+                        Assembly assembly = Assembly.LoadFrom(assemblyFile);
+                        m.HbmMappings.AddFromAssembly(assembly);
+                        m.FluentMappings.AddFromAssembly(assembly).Conventions.AddAssembly(assembly);
+                    });
+                    return fluentConfiguration.BuildSessionFactory();
+                }
+                else
+                {
+                    return sessionFactory;
+                }
+            }
+        }
+
+        public static ISession CreateSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+        
+    }
+}
---
+++ KeWeiOMS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KeWeiOMS.Web.MvcApplication" Language="C#" %>
