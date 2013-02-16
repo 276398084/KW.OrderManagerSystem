985a275d326a37eba252b52cd7128fdc1a9f889c
jmyd <jmyd@live.cn>
2013-02-16 09:48:57 +0800

    新商品导购

---
+++ KeWeiOMS.Domain/Entities/NewGuideType.cs
@@ -0,0 +1,89 @@
+﻿//--------------------------------------------------------------------
+// All Rights Reserved , Copyright (C)  , KeWei TECH, Ltd.
+//--------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace KeWeiOMS.Domain
+{
+
+    /// <summary>
+    /// NewGuideType
+    /// 新产品导购
+    /// 
+    /// 修改纪录
+    /// 
+    ///  版本：1.0  创建主键。
+    /// 
+    /// 版本：1.0
+    /// 
+    /// <author>
+    /// <name></name>
+    /// <date></date>
+    /// </author>
+    /// </summary>
+    public class NewGuideType
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// 新SKU
+        /// </summary>
+        public virtual String NewSku { get; set; }
+
+        /// <summary>
+        /// 旧SKU
+        /// </summary>
+        public virtual String OldSku { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public virtual String Title { get; set; }
+
+        /// <summary>
+        /// 图片
+        /// </summary>
+        public virtual String Pic { get; set; }
+
+        /// <summary>
+        /// 网址
+        /// </summary>
+        public virtual String Url { get; set; }
+
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public virtual double Price { get; set; }
+
+        /// <summary>
+        /// 规格
+        /// </summary>
+        public virtual String ColorSize { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public virtual String Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public virtual String Status { get; set; }
+
+        /// <summary>
+        /// 采购标记
+        /// </summary>
+        public virtual int IsCheck { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public virtual DateTime CreateOn { get; set; }
+
+    }
+}
---
+++ KeWeiOMS.Domain/Mappings/NewGuideTypeMap.cs
@@ -0,0 +1,46 @@
+﻿//--------------------------------------------------------------------
+// All Rights Reserved , Copyright (C)  , KeWei TECH, Ltd.
+//--------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using FluentNHibernate.Mapping;
+
+namespace KeWeiOMS.Domain
+{
+
+    /// <summary>
+    /// NewGuideTypeMap
+    /// 新产品导购
+    /// 
+    /// 修改纪录
+    /// 
+    ///  版本：1.0  创建主键。
+    /// 
+    /// 版本：1.0
+    /// 
+    /// <author>
+    /// <name></name>
+    /// <date></date>
+    /// </author>
+    /// </summary>
+    public class NewGuideTypeMap : ClassMap<NewGuideType> 
+    {
+        public NewGuideTypeMap()
+        {
+            Table("NewGuide");
+            Id(x => x.Id);
+            Map(x => x.NewSku).Length(50);
+            Map(x => x.OldSku).Length(50);
+            Map(x => x.Title).Length(400);
+            Map(x => x.Pic).Length(400);
+            Map(x => x.Url).Length(400);
+            Map(x => x.Price);
+            Map(x => x.ColorSize).Length(50);
+            Map(x => x.Remark).Length(50);
+            Map(x => x.Status).Length(50);
+            Map(x => x.IsCheck);
+            Map(x => x.CreateOn);
+        }
+    }
+}
