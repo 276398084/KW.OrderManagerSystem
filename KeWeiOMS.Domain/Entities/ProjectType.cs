9e3d690e208fde64385f7560d41d509d8cdec253
blankge <cnblank@hotmail.com>
2013-03-05 10:30:19 +0800

    采购计划搜索添加

---
+++ KeWeiOMS.Domain/Entities/ProjectType.cs
@@ -0,0 +1,89 @@
+﻿//--------------------------------------------------------------------
+// All Rights Reserved , Copyright (C)  , KeWei TECH, Ltd.
+//--------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace KeWeiOMS.Domain
+{
+
+    /// <summary>
+    /// ProjectType
+    /// 项目管理
+    /// 
+    /// 修改纪录
+    /// 
+    ///  版本：1.0  创建主键。
+    /// 
+    /// 版本：1.0
+    /// 
+    /// <author>
+    /// <name></name>
+    /// <date></date>
+    /// </author>
+    /// </summary>
+    public class ProjectType
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public virtual String ProjectName { get; set; }
+
+        /// <summary>
+        /// 状态（未开始，执行中，暂停中，已完成）
+        /// </summary>
+        public virtual String Status { get; set; }
+
+        /// <summary>
+        /// 项目负责人
+        /// </summary>
+        public virtual String ProjectManager { get; set; }
+
+        /// <summary>
+        /// 项目进程
+        /// </summary>
+        public virtual String Content { get; set; }
+
+        /// <summary>
+        /// 项目开始时间
+        /// </summary>
+        public virtual DateTime BeginDate { get; set; }
+
+        /// <summary>
+        /// 预计完成时间
+        /// </summary>
+        public virtual DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// 工期
+        /// </summary>
+        public virtual double NeedDay { get; set; }
+
+        /// <summary>
+        /// 实际完成时间
+        /// </summary>
+        public virtual DateTime RealEndDate { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public virtual DateTime CreateOn { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public virtual String CreateBy { get; set; }
+
+        /// <summary>
+        /// 评价
+        /// </summary>
+        public virtual String Evaluate { get; set; }
+
+    }
+}
---
+++ KeWeiOMS.Domain/Mappings/ProjectTypeMap.cs
@@ -0,0 +1,46 @@
+﻿//--------------------------------------------------------------------
+// All Rights Reserved , Copyright (C)  , KeWei TECH, Ltd.
+//--------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using FluentNHibernate.Mapping;
+
+namespace KeWeiOMS.Domain
+{
+
+    /// <summary>
+    /// ProjectTypeMap
+    /// 项目管理
+    /// 
+    /// 修改纪录
+    /// 
+    ///  版本：1.0  创建主键。
+    /// 
+    /// 版本：1.0
+    /// 
+    /// <author>
+    /// <name></name>
+    /// <date></date>
+    /// </author>
+    /// </summary>
+    public class ProjectTypeMap : ClassMap<ProjectType> 
+    {
+        public ProjectTypeMap()
+        {
+            Table("Projects");
+            Id(x => x.Id);
+            Map(x => x.ProjectName).Length(100);
+            Map(x => x.Status).Length(50);
+            Map(x => x.ProjectManager).Length(100);
+            Map(x => x.Content).Length(2000);
+            Map(x => x.BeginDate);
+            Map(x => x.EndDate);
+            Map(x => x.NeedDay);
+            Map(x => x.RealEndDate);
+            Map(x => x.CreateOn);
+            Map(x => x.CreateBy).Length(200);
+            Map(x => x.Evaluate).Length(200);
+        }
+    }
+}
