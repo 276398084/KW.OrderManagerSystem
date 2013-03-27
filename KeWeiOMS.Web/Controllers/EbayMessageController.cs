ebdf5077ac6a4e4683cddce8f5dda82a88eea073
blankge <cnblank@hotmail.com>
2013-03-27 17:32:36 +0800

    修改

---
+++ KeWeiOMS.Web/Controllers/EbayMessageController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using KeWeiOMS.Domain;
+using KeWeiOMS.NhibernateHelper;
+using NHibernate;
+
+namespace KeWeiOMS.Web.Controllers
+{
+    public class EbayMessageController : BaseController
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Create(EbayMessageType obj)
+        {
+            try
+            {
+                NSession.SaveOrUpdate(obj);
+                NSession.Flush();
+            }
+            catch (Exception ee)
+            {
+                return Json(new { errorMsg = "出错了" });
+            }
+            return Json(new { IsSuccess = "true" });
+        }
+
+        /// <summary>
+        /// 根据Id获取
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public  EbayMessageType GetById(int Id)
+        {
+            EbayMessageType obj = NSession.Get<EbayMessageType>(Id);
+            if (obj == null)
+            {
+                throw new Exception("返回实体为空");
+            }
+            else
+            {
+                return obj;
+            }
+        }
+
+        [OutputCache(Location = OutputCacheLocation.None)]
+        public ActionResult Edit(int id)
+        {
+            EbayMessageType obj = GetById(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        [OutputCache(Location = OutputCacheLocation.None)]
+        public ActionResult Edit(EbayMessageType obj)
+        {
+           
+            try
+            {
+                NSession.Update(obj);
+                NSession.Flush();
+            }
+            catch (Exception ee)
+            {
+                return Json(new { errorMsg = "出错了" });
+            }
+            return Json(new { IsSuccess = "true" });
+           
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public JsonResult DeleteConfirmed(int id)
+        {
+          
+            try
+            {
+                EbayMessageType obj = GetById(id);
+                NSession.Delete(obj);
+                NSession.Flush();
+            }
+            catch (Exception ee)
+            {
+                return Json(new { errorMsg = "出错了" });
+            }
+            return Json(new { IsSuccess = "true" });
+        }
+
+        public JsonResult List(int page, int rows)
+        {
+            IList<EbayMessageType> objList = NSession.CreateQuery("from EbayMessageType")
+                .SetFirstResult(rows * (page - 1))
+                .SetMaxResults(rows * page)
+                .List<EbayMessageType>();
+
+            return Json(new { total = objList.Count, rows = objList });
+        }
+
+    }
+}
+
