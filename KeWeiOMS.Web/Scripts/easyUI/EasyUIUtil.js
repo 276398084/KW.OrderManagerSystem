7c961438128c7daeadb01418fdad1183a4948f2f
mccj <mccj@qq.com>
2013-11-25 16:21:30 +0800

    增加多语言支持

--- KeWeiOMS.Web/Scripts/easyUI/EasyUIUtil.js
+++ KeWeiOMS.Web/Scripts/easyUI/EasyUIUtil.js
@@ -10,6 +10,14 @@
  *            主题名称
  */
 changeTheme = function (themeName) {
+    //delCookie('Theme');
+    //SetCookie('Theme', themeName);
+    $.cookie('Theme', themeName);
+    setTheme(themeName);
+};
+setTheme = function (themeName) {
+    var themeName = $.cookie('Theme');
+    if (themeName == null) { themeName = "metro-blue"; }
     var $easyuiTheme = $('#easyuiTheme');
     var url = $easyuiTheme.attr('href');
     var href = url.substring(0, url.indexOf('themes')) + 'themes/' + themeName + '/easyui.css';
@@ -22,34 +30,36 @@ changeTheme = function (themeName) {
             $(ifr).contents().find('#easyuiTheme').attr('href', href);
         }
     }
-    delCookie('Theme');
-    SetCookie('Theme', themeName);
-};
-function SetCookie(name,value)//两个参数，一个是cookie的名子，一个是值
 
-    {
-        var Days =30; //此 cookie 将被保存 30 天
-
-        var exp = new Date();    //new Date("December 31, 9998");
-
-        exp.setTime(exp.getTime() + Days*24*60*60*1000);
-
-        document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + ";path=/";;
+    $("#layout_north_pfMenu .menu-item").removeClass("accordion-expand");
+    $("#layout_north_pfMenu .menu-item[l=" + themeName + "]").addClass("accordion-expand");
 }
 
+//function SetCookie(name,value)//两个参数，一个是cookie的名子，一个是值
 
-function delCookie(name)//删除cookie
-    {
-        var exp = new Date();
+//    {
+//        var Days =30; //此 cookie 将被保存 30 天
 
-        exp.setTime(exp.getTime() - 1);
+//        var exp = new Date();    //new Date("December 31, 9998");
 
-        var cval=getCookie(name);
+//        exp.setTime(exp.getTime() + Days*24*60*60*1000);
 
-        if (cval != null)
-            document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString() + "; path=/";;
+//        document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + ";path=/";;
+//}
 
-    }
+
+//function delCookie(name)//删除cookie
+//    {
+//        var exp = new Date();
+
+//        exp.setTime(exp.getTime() - 1);
+
+//        var cval=getCookie(name);
+
+//        if (cval != null)
+//            document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString() + "; path=/";;
+
+//    }
 
 
 
